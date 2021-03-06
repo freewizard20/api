de82a397143a648695672b3f4e2bdbacb937d619
freewizard20 <gseongkyu79@gmail.com>
2021-03-06 22:46:03 +0900

    added reload functionality

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views','./views');
 
 app.get('/',(req,res)=>{
-	res.render('home',{bitcoin:30000,ethereum:3000,kimchi:2.89,exchange:0.03});
+	const bitcoin = axios.get('https://ftx.com/api/markets/BTC/USD');
+	const ethereum = axios.get('https://ftx.com/api/markets/ETH/USD');
+	const bitcoin_krw = axios.get('https://api.bithumb.com/public/ticker/BTC_KRW');
+	const exchange = axios.get('https://ftx.com/api/markets/ETH/BTC');
+	const usd = axios.get('https://api.exchangeratesapi.io/latest?base=USD');
+
+	axios.all([bitcoin,ethereum,bitcoin_krw,exchange,usd]).then(axios.spread((...responses)=>{
+		const resultbitcoin = responses[0];
+		const resultethereum = responses[1];
+		const resultbitcoin_krw = responses[2].data.data.closing_price;
+		const resultexchange = responses[3];
+		const usd = Math.round(responses[4].data.rates.KRW);
+		const kimchi = (resultbitcoin_krw - (resultbitcoin.data.result.last * usd))* 100 / resultbitcoin_krw
+
+		res.render('home',{bitcoin:resultbitcoin.data.result.last,ethereum:resultethereum.data.result.last,kimchi:kimchi.toFixed(2),exchange:resultexchange.data.result.last});
+	}));
 });
 
 app.get('/bitcoin',(req,res)=>{
