aa725e0dbd3025fd625f37da1eb680551718ef17
freewizard20 <gseongkyu79@gmail.com>
2021-03-06 22:06:19 +0900

    basic layout of api

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.get('/',(req,res)=>{
-	res.send('Hello world from api');
-	console.log('/ entered api');
+	res.render('home',{bitcoin:30000,ethereum:3000,kimchi:2.89,exchange:0.03});
+});
+
+app.get('/bitcoin',(req,res)=>{
+	res.render('bitcoin',{data:[]});
+});
+
+app.get('/ethereum',(req,res)=>{
+	res.render('ethereum',{data:[]});
+});
+
+app.get('/kimp',(req,res)=>{
+	res.render('kimp',{data:[]});
 });
 
 app.listen(3001, ()=>{
